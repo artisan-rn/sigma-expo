486e167044cf4c210e5616735cdad309a8a11054
zhhyang <zhhyang0404@163.com>
2019-09-28 11:56:17 +0800

    Updates

---
+++ example/App.tsx
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  Platform,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import {routers} from './routers';
+
+const {width} = Dimensions.get('window');
+
+const COLORS = [
+  '#FF4777',
+  '#725E82',
+  '#FFA400',
+  '#5D513C',
+  '#00BC12',
+  '#CA6924',
+  '#CB3A56',
+];
+
+export default class App extends Component {
+
+  static navigationOptions = {
+    title: 'react-native组件库',
+  };
+
+  state = {
+    modalVisible: false,
+    selectedComponentIndex: -1,
+  };
+
+  onCloseModal = () => {
+    this.setState({
+      modalVisible: false,
+      selectedComponentIndex: -1,
+    });
+  };
+
+  onPressCard(item) {
+    this.props.navigation.navigate(item.key);
+  }
+
+  renderItem = ({item, index}) => {
+    const {key} = item;
+    return (
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => this.onPressCard(item)}>
+        <View
+          style={[
+            styles.badgeWrapper,
+            {backgroundColor: COLORS[index % COLORS.length]},
+          ]}
+        />
+        <Text style={styles.cardTitle}>{key}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const {} = this.state;
+    return (
+      <React.Fragment>
+        <FlatList
+          numColumns={2}
+          data={routers}
+          style={styles.list}
+          keyExtractor={(_, idx) => idx}
+          renderItem={this.renderItem}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+    padding: 5,
+  },
+  card: {
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: (width - 30) / 2,
+    height: (width - 30) / 2,
+    backgroundColor: '#FFF',
+    ...Platform.select({
+      android: {
+        elevation: 2,
+      },
+      ios: {
+        shadowColor: '#000',
+        shadowOpacity: 0.15,
+        shadowOffset: {width: 0, height: 0},
+        shadowRadius: 5,
+      },
+    }),
+  },
+  badgeWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  badgeText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  cardTitle: {
+    marginTop: 15,
+    fontSize: 15,
+    color: '#333',
+    textAlign: 'center',
+  },
+});
---
+++ example/Button/index.tsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+import {View, StyleSheet} from 'react-native';
+
+import {Button} from '@sigma/rn';
+import Card from '../Card';
+
+export default class ButtonScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card title="常规" style={{flexDirection: 'row'}}>
+          <Button size="lg">去结算</Button>
+          <Button size="lg" disabled>
+            去结算
+          </Button>
+        </Card>
+        <Card title="常规-小尺寸" style={{flexDirection: 'row'}}>
+          <Button size="md">去结算</Button>
+          <Button size="md" disabled>
+            去结算
+          </Button>
+        </Card>
+        <Card title="通栏按钮">
+          <Button block>登录</Button>
+        </Card>
+        <Card title="圆角按钮-常规" style={{flexDirection: 'row'}}>
+          <Button>确认收货</Button>
+          <Button type="ghost">再次购买</Button>
+          <Button type="default">查看物流</Button>
+          <Button size="xs" type="fill">
+            降价通知
+          </Button>
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    paddingVertical: 7.5,
+  },
+  btn: {
+    marginRight: 15,
+    marginVertical: 7.5,
+  },
+});
